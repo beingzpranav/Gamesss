4144fb2e9e52616e817bd200bbbf772f1cb293f9
Pranav Khandelwal <khandelwalpranav86@gmail.com>
2026-04-22 21:41:36 +0530

    fix: Update CORS configuration to allow multiple origins

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://imposter.pranavk.tech"],
     methods: ["GET", "POST"]
   }
 });
